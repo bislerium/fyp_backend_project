0e9df34bbce66bda7024e2cf0b781bb30c97f7af
Bishal Gharti Chhetri <chhetrig.bishal@gmail.com>
2022-03-17 13:01:00 +0545

    Upvote downvote api logic implementation

--- core/api_views.py
+++ core/api_views.py
@@ -11,6 +11,11 @@ from rest_framework.views import APIView
 from .serializers import *
 
 
+class EReactionType(enum.Enum):
+    Upvote = 0,
+    Downvote = 1,
+
+
 class CustomLoginView(LV):
     serializer_class = CustomLoginSerializer
 
@@ -100,3 +105,55 @@ def post_a_post(self, request, post_type: EPostType):
 class PostDetail(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class ToggleUpvoteView(APIView):
+
+    def post(self, request, post_id):
+        return react_post(request.user, post_id, EReactionType.Upvote)
+
+
+class ToggleDownvoteView(APIView):
+
+    def post(self, request, post_id):
+        return react_post(request.user, post_id, EReactionType.Downvote)
+
+
+def react_post(user, post_id, reaction_type: EReactionType):
+    if user.groups.first().name != 'General':
+        return Response({"Fail": f'Only general people can {reaction_type.name.lower()} the normal post!'},
+                        status=status.HTTP_403_FORBIDDEN)
+    filtered_post = Post.objects.filter(id=post_id)
+    if not filtered_post.exists():
+        return Response({"Fail": 'Normal post with given post id not found!'}, status=status.HTTP_404_NOT_FOUND)
+    post = filtered_post.first()
+    if post.post_type != 'Normal':
+        return Response({"Fail": f'Can only {reaction_type.name.lower()} normal post!'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    match reaction_type:
+        case EReactionType.Upvote:
+            reaction_queryset = post.postnormal.up_vote.all()
+        case EReactionType.Downvote:
+            reaction_queryset = post.postnormal.down_vote.all()
+    if user.peopleuser in reaction_queryset:
+        match reaction_type:
+            case EReactionType.Upvote: post.postnormal.up_vote.remove(user.peopleuser)
+            case EReactionType.Downvote: post.postnormal.down_vote.remove(user.peopleuser)
+        return Response({"Undone": f'Normal post {reaction_type.name.lower()} undone.'}, status=status.HTTP_200_OK)
+    else:
+        match reaction_type:
+            case EReactionType.Upvote:
+                post.postnormal.up_vote.add(user.peopleuser)
+                post.postnormal.down_vote.remove(user.peopleuser)
+            case EReactionType.Downvote:
+                post.postnormal.down_vote.add(user.peopleuser)
+                post.postnormal.up_vote.remove(user.peopleuser)
+        return Response({"Done": f'Normal post {reaction_type.name.lower()} done.'}, status=status.HTTP_200_OK)
+
+
+class PostReportView(APIView):
+    pass
+
+
+class RequestPostParticipateView(APIView):
+    pass
--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth.views import *
 from django.urls import path, include
 
 from core.api_views import NGODetail, PostList, PostDetail, PeopleList, PeopleDetail, BankDetail, NGOList, \
-    CustomLoginView, PeopleAdd, NormalPostAdd, PollPostAdd, RequestPostAdd
+    CustomLoginView, PeopleAdd, NormalPostAdd, PollPostAdd, RequestPostAdd, ToggleUpvoteView, ToggleDownvoteView, \
+    PostReportView, RequestPostParticipateView
 from core.serializers import PostNormalSerializer, PostRequestSerializer, PostPollSerializer
 
 from core.views import *
@@ -90,7 +91,10 @@ urlpatterns = [
                   path('api/post/poll', PollPostAdd.as_view(), name='api-poll-post-add'),
                   path('api/post/request', RequestPostAdd.as_view(), name='api-request-post-add'),
                   path('api/posts/', PostList.as_view(), name='api-post-list'),
-                  path('api/post/<int:pk>/', PostDetail.as_view(), name='api-post-detail'),
+                  path('api/post/<int:post_id>/upvote', ToggleUpvoteView.as_view(), name='api-post-upvote'),
+                  path('api/post/<int:post_id>/downvote', ToggleDownvoteView.as_view(), name='api-post-downvote'),
+                  path('api/post/<int:post_id>/participate', RequestPostParticipateView.as_view(), name='api-post-report'),
+                  path('api/post/<int:post_id>/report', PostReportView.as_view(), name='api-post-report'),
 
                   path('api/people/', PeopleList.as_view(), name='api-people-list'),
                   path('api/people/add/', PeopleAdd.as_view(), name='api-people-add'),
