03678d0ab36aacf3dbcb0f485ad209d6f152807c
Bishal Gharti Chhetri <chhetrig.bishal@gmail.com>
2022-08-09 17:59:26 +0545

    retry

--- fyp_backend/settings.py
+++ fyp_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['asasae.herokuapp.com', 'localhost', '127.0.0.1']
 
