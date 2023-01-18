34df0709e4df60bc4fdb0471b40fade5862a400c
Bishal Gharti Chhetri <chhetrig.bishal@gmail.com>
2023-01-19 02:32:28 +0545

    update

--- fyp_backend/settings.py
+++ fyp_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default="")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sasae.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['sasae.up.railway.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
